e2658a27dd69f6b56dad027fb95941b0d09e73f3
Mateusz Stankowski <m.stankowski16@gmail.com>
2021-09-02 03:12:17 +0200

    24.3 Aspect programming in Spring Boot, assignment

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
@@ -1,6 +1,7 @@
 package com.kodilla.patterns2.facade.api;
 
 import com.kodilla.patterns2.facade.ShopService;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,12 @@ public class OrderFacade {
     @Autowired
     public OrderFacade(ShopService shopService) {
         this.shopService = shopService;
+    }
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))" +
+            "&& args(order,userId) && target(object)")
+    public void logEvent(OrderDto order, Long userId, Object object) {
+        LOGGER.info("Class: " + object.getClass().getName() + ", Args: " + order.getItems().size() + " " + userId);
     }
 
     public void processOrder(final OrderDto order, final Long userId) throws OrderProcessingException {
